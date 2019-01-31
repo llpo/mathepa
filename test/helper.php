cd5c83df1898355d6edc5c71daa8fa7dd6945a93
Lluís Pons <lpo@posteo.de>
2019-01-31 18:47:07 +0100

    Introduce "pr" helper to dump a variable state

--- test/helper.php
+++ test/helper.php
@@ -1,6 +1,9 @@
 <?php
 
-namespace Mathepa\Test;
+// Helpers
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 
 /**
  * Calls a protected/private method of a class.
@@ -10,11 +13,22 @@ namespace Mathepa\Test;
  * @param array  $parameters Array of parameters to pass into method.
  * @return mixed
  */
-function invokeMethod(&$object, $methodName, array $parameters = array())
+function invokeMethod(&$object, $methodName, array $parameters = [])
 {
     $reflection = new \ReflectionClass(get_class($object));
     $method = $reflection->getMethod($methodName);
     $method->setAccessible(true);
 
     return $method->invokeArgs($object, $parameters);
+}
+
+/**
+ * @param string $var
+ * @return void
+ */
+function pr(...$var): void
+{
+    $cloner = new VarCloner();
+    $dumper = 'cli' === PHP_SAPI ? new CliDumper() : new HtmlDumper();
+    $dumper->dump($cloner->cloneVar($var));
 }
